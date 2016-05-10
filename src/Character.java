f66c00766db5b3234a8f43d0e17c69a376f27a69
FermatPredictor <forclass021117@gmail.com>
2016-05-10 21:15:17 +0800

    0.0

---
+++ src/Character.java
@@ -0,0 +1,32 @@
+
+public class Character {
+	
+	private int MAX_HP,now_HP;
+	private String name;
+	private int x,y;
+	private int velocityForDirectionY;
+	private int gravity;
+	
+	public Character(){
+		
+	}
+	
+	//the effect of gravity that make character falldown
+    public void fallDown(){
+			
+	}
+	
+    public void move(String direction){
+		
+	}
+	
+	public void jump(){
+		
+	}
+	
+    public void attack(){
+		
+	}
+	
+
+}

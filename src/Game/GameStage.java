853884c2a90235334aab17d2c642cbc75c911f52
hsyhsyh <s9661009@gmail.com>
2016-05-14 22:01:21 +0800

    完成PApplet架構

--- src/Game/GameStage.java
+++ src/Game/GameStage.java
@@ -1,68 +1,33 @@
 package Game;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.image.BufferedImage;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-
-import javax.imageio.ImageIO;
 
 import processing.core.PApplet;
+import processing.core.PImage;
 
-public class GameStage extends PApplet implements Runnable {
+public class GameStage extends PApplet {
 	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-	BufferedImage duck = null;
+	private final static int width = 1000, height = 500;
 	
-	public GameStage() {
+	private PImage duck;
+	
+	public void setup() {
 		
-		setLayout(null);
-		//setBackground(Color.cyan);
-		setVisible(false);
-		setBounds(0, 0, 1000, 500);
-		Thread GameStage = new Thread(this);
-		GameStage.start();
-		
-		try {
-			duck = ImageIO.read(new File("duck.png"));
-			
-		} catch (IOException e) {
-		}
-		setVisible(true);
+		size(width, height);
+		smooth();
+		this.duck = loadImage("duck.png");
 		
 	}
-
-//	protected void paintComponent(Graphics g) {
-//		
-//		super.paintComponent(g);
-//		g.drawImage(this.duck, 50, 50, this	);
-//		g.setColor(Color.BLACK);
-//		g.fillRect(0, 0, 1000, 50);
-//		g.fillRect(0, 420, 1000, 50);
-//		g.fillRect(0, 50, 50, 370);
-//		g.fillRect(950, 50, 50, 370);
-//	}
-
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-
-		try {
-			while(true){
-				this.repaint();
-		        Thread.sleep(40);
-			}
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	
+	public void draw() {
+		
+		background(255);
+        image(this.duck, 100, 100);
+        stroke(0);
+        fill(0);
+        this.rect(0, 0, 1000, 50);
+        this.rect(0, 420, 1000, 50);
+        this.rect(0, 50, 50, 370);
+        this.rect(950, 50, 50, 370);
+        
 	}
-
 }
--- src/Game/Main.java
+++ src/Game/Main.java
@@ -1,14 +1,24 @@
 package Game;
 import javax.swing.JFrame;
 
-public class Main {
-	public static void main(String[] args) {
-		MyWindow window = new MyWindow();
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setSize(1000, 500);
-		window.setResizable(false);
-		window.setVisible(true);
+@SuppressWarnings("serial")
+public class Main extends JFrame{
+	
+	private final static int windowWidth = 1000, windowHeight = 500;
+	
+	public static void main(String [] args){
 		
+		GameStage applet = new GameStage();
+		applet.init();
+		applet.start();
+		applet.setFocusable(true);
+		
+		JFrame window = new JFrame("Shadow-World");
+		window.setLocation(100, 50);
+		window.setContentPane(applet);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setSize(windowWidth, windowHeight);
+		window.setVisible(true);
+		window.setResizable(false);
 	}
-
 }

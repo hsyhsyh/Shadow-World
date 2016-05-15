e8d7a08cf83ae129117c83190c14f989fc290eb5
hsyhsyh <s9661009@gmail.com>
2016-05-15 12:31:40 +0800

    請更新 >_#

--- src/Game/GameStage.java
+++ src/Game/GameStage.java
@@ -8,20 +8,22 @@ public class GameStage extends PApplet {
 	private static final long serialVersionUID = 1L;
 	private final static int width = 1000, height = 500;
 	
-	private PImage man2;
+	private PImage man, books;
 	
 	public void setup() {
 		
 		size(width, height);
 		smooth();
-		this.man2 = loadImage("man2.jpg");
+		this.books = loadImage("books.jpg");
+		this.man = loadImage("man2.jpg");
 		
 	}
 	
 	public void draw() {
 		
 		background(255);
-        image(this.man2, 100, 100);
+        image(this.books, 200, 100);
+        image(this.man, 600, 300);
         stroke(0);
         fill(0);
         this.rect(0, 0, 1000, 50);

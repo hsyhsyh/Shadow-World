821fe2b950ab0fe52a84b7ddd343f1d103ee10d3
hsyhsyh <s9661009@gmail.com>
2016-05-17 19:26:49 +0800

    圖片檔大致完成

--- src/Game/GameStage.java
+++ src/Game/GameStage.java
@@ -8,7 +8,7 @@ public class GameStage extends PApplet {
 	private static final long serialVersionUID = 1L;
 	private final static int width = 1000, height = 500;
 	
-	private PImage man, books;
+	private PImage man, books, monster, strike, box, bed, lader, door1;
 	
 	public void setup() {
 		
@@ -16,6 +16,12 @@ public class GameStage extends PApplet {
 		smooth();
 		this.books = loadImage("books.jpg");
 		this.man = loadImage("man2.jpg");
+		this.monster = loadImage("monster.jpg");
+		this.strike = loadImage("strike.png");
+		this.box = loadImage("box.png");
+		this.bed = loadImage("bed.jpg");
+		this.lader = loadImage("lader.png");
+		this.door1 = loadImage("opendoor.png");
 		
 	}
 	
@@ -23,7 +29,13 @@ public class GameStage extends PApplet {
 		
 		background(255);
         image(this.books, 200, 100);
+        image(this.bed, 600, 50);
+        image(this.lader, 600, 100);
+        image(this.strike, 800, 330);
+        image(this.box, 200, 330);
         image(this.man, 600, 300);
+        image(this.monster, 650, 300);
+        image(this.door1, 80, 300);
         stroke(0);
         fill(0);
         this.rect(0, 0, 1000, 50);

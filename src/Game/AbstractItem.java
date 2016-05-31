82263bd90e9d450c21afcbb125f3e6892035133e
吳俊德 <oojimda3838@hotmail.com>
2016-05-31 16:14:03 +0800

    ???

--- src/Game/AbstractItem.java
+++ src/Game/AbstractItem.java
@@ -6,13 +6,13 @@ abstract public class AbstractItem {
 
 	public float x,y;
 	private int width,height;
-	private String[] text;
+	private String text;
 	private PImage image;
-	public String[] gettext()
+	public String gettext()
 	{
 		return text;
 	}
-	public void settext(String[] s)
+	public void settext(String s)
 	{
 		text = s;
 	}
